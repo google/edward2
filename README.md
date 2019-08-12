d0fe8be3dabd9a785c52609aeb81d899052b0a64
Dustin Tran <trandustin@google.com>
2019-08-12 23:42:59 +0000

    Project import generated by Copybara.
    PiperOrigin-RevId: 263032314

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# edward2
+# Edward2
+
+This file is to be replaced by [this README.md](
+https://github.com/https://github.com/google-research/google-research/tree/master/simple_probabilistic_programming/README.md)
---
+++ oss_scripts/oss_pip_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -v  # print commands as they're executed
+set -e  # fail and exit on any command erroring
+
+: "${TF_VERSION:?}"
+
+if [[ "$TF_VERSION" == "tf-nightly"  ]]
+then
+  pip install tf-nightly;
+else
+  pip install -q "tensorflow==$TF_VERSION"
+fi
+
+# Make sure we have the latest version of numpy - avoid problems we were
+# seeing with Python 3
+pip install -q -U numpy
+
+# First ensure that the base dependencies are sufficient for a full import
+pip install -q -e .
+python -c "import edward2 as ed"
+
+# Then install the test dependencies
+pip install -q -e .[tests]
