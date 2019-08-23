12f114c46b7e07b05077b9534058b6b512adebd2
Dustin Tran <dustinviettran@gmail.com>
2019-08-23 12:11:54 -0700

    Make constraints.py use the TF2 namespace.

--- edward2/tensorflow/constraints.py
+++ edward2/tensorflow/constraints.py
@@ -20,7 +20,7 @@ from __future__ import division
 from __future__ import print_function
 
 import six
-import tensorflow as tf
+import tensorflow.compat.v2 as tf
 
 
 class Positive(tf.keras.constraints.Constraint):
