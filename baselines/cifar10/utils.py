1692ccbf44d879ace058c247861a880cf8fd335c
Ghassen Jerfel <ghassen@google.com>
2019-12-04 13:26:35 -0800

    Add diversity metrics.
    PiperOrigin-RevId: 283822618

--- baselines/cifar10/utils.py
+++ baselines/cifar10/utils.py
@@ -78,3 +78,86 @@ def make_lr_scheduler(init_lr):
     logging.info('Learning rate=%f for epoch=%d ', rate, epoch)
     return rate
   return tf.keras.callbacks.LearningRateScheduler(schedule_fn)
+
+
+def disagreement(logits_1, logits_2):
+  """Disagreement between the predictions of two classifiers."""
+  preds_1 = tf.argmax(logits_1, axis=-1, output_type=tf.int32)
+  preds_2 = tf.argmax(logits_2, axis=-1, output_type=tf.int32)
+  return tf.reduce_mean(tf.cast(preds_1 != preds_2, tf.float32))
+
+
+def double_fault(logits_1, logits_2, labels):
+  """Double fault[1] is the number of examples both classifiers predict wrong.
+
+  Args:
+    logits_1: tf.Tensor.
+    logits_2: tf.Tensor.
+    labels: tf.Tensor.
+
+  Returns:
+    Scalar double-fault diversity metric.
+
+  ## References
+
+  [1] Kuncheva, Ludmila I., and Christopher J. Whitaker. "Measures of diversity
+      in classifier ensembles and their relationship with the ensemble
+      accuracy." Machine learning 51.2 (2003): 181-207.
+  """
+  preds_1 = tf.cast(tf.argmax(logits_1, axis=-1), labels.dtype)
+  preds_2 = tf.cast(tf.argmax(logits_2, axis=-1), labels.dtype)
+
+  fault_1_idx = tf.squeeze(tf.where(preds_1 != labels))
+  fault_1_idx = tf.cast(fault_1_idx, tf.int32)
+
+  preds_2_at_idx = tf.gather(preds_2, fault_1_idx)
+  labels_at_idx = tf.gather(labels, fault_1_idx)
+
+  double_faults = preds_2_at_idx != labels_at_idx
+  double_faults = tf.cast(double_faults, tf.float32)
+  return tf.reduce_mean(double_faults)
+
+
+def average_kl_divergence(logits_1, logits_2):
+  """Average KL divergence between logit distributions of two classifiers."""
+  probs_1 = tf.nn.softmax(logits_1)
+  probs_2 = tf.nn.softmax(logits_2)
+  vals = kl_divergence(probs_1, probs_2)
+  return tf.reduce_mean(vals)
+
+
+def kl_divergence(p, q):
+  """Generalized KL divergence [1] for unnormalized distributions.
+
+  Args:
+    p: tf.Tensor.
+    q: tf.Tensor
+
+  Returns:
+    tf.Tensor of the Kullback-Leibler divergences per example.
+
+  ## References
+
+  [1] Lee, Daniel D., and H. Sebastian Seung. "Algorithms for non-negative
+  matrix factorization." Advances in neural information processing systems.
+  2001.
+  """
+  return tf.reduce_sum(p * tf.math.log(p / q) - p + q, axis=-1)
+
+
+def lp_distance(x, y, p=1):
+  """l_p distance."""
+  diffs_abs = tf.abs(x - y)
+  summation = tf.reduce_sum(tf.math.pow(diffs_abs, p), axis=-1)
+  return tf.reduce_mean(tf.math.pow(summation, 1./p), axis=-1)
+
+
+def cosine_distance(x, y):
+  """Cosine distance between vectors x and y."""
+  x_norm = tf.math.sqrt(tf.reduce_sum(tf.pow(x, 2), axis=-1))
+  x_norm = tf.reshape(x_norm, (-1, 1))
+  y_norm = tf.math.sqrt(tf.reduce_sum(tf.pow(y, 2), axis=-1))
+  y_norm = tf.reshape(y_norm, (-1, 1))
+  normalized_x = x / x_norm
+  normalized_y = y / y_norm
+  return tf.reduce_sum(normalized_x * normalized_y)
