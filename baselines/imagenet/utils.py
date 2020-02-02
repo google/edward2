91b06a0bd9d1b3f3141f0e44d6e0c5d3013b7412
Ghassen Jerfel <ghassen@google.com>
2020-02-01 19:10:10 -0800

    Added diversity metrics for ImageNet.
    PiperOrigin-RevId: 292747046

--- baselines/imagenet/utils.py
+++ baselines/imagenet/utils.py
@@ -19,7 +19,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import itertools
 import os
+
+import numpy as np
 import tensorflow.compat.v1 as tf
 import tensorflow_datasets as tfds
 
@@ -401,3 +404,164 @@ def corrupt_test_input_fn(corruption_name,
     return dataset
 
   return test_input_fn
+
+
+def aggregate_corrupt_metrics(metrics, corruption_types, max_intensity):
+  """Aggregates metrics across intensities and corruption types."""
+  # TODO(trandustin): Decide whether to stick with mean or median; showing both
+  # for now.
+  results = {'test/nll_mean_corrupted': 0.,
+             'test/error_mean_corrupted': 0.,
+             'test/ece_mean_corrupted': 0.,
+             'test/nll_median_corrupted': 0.,
+             'test/error_median_corrupted': 0.,
+             'test/ece_median_corrupted': 0.}
+  for intensity in range(1, max_intensity + 1):
+    ece = np.zeros(len(corruption_types))
+    nll = np.zeros(len(corruption_types))
+    acc = np.zeros(len(corruption_types))
+    for i in range(len(corruption_types)):
+      dataset_name = '{0}_{1}'.format(corruption_types[i], intensity)
+      nll[i] = metrics['test/nll_{}'.format(dataset_name)].result()
+      acc[i] = metrics['test/accuracy_{}'.format(dataset_name)].result()
+      ece[i] = metrics['test/ece_{}'.format(dataset_name)].result()
+    avg_nll = np.mean(nll)
+    avg_error = 100 * (1 - 1. * np.mean(acc))
+    avg_ece = np.mean(ece)
+    median_nll = np.median(nll)
+    median_error = 100 * (1. - 1. * np.median(acc))
+    median_ece = np.median(ece)
+    results['test/nll_mean_{}'.format(intensity)] = avg_nll
+    results['test/error_mean_{}'.format(intensity)] = avg_error
+    results['test/ece_mean_{}'.format(intensity)] = avg_ece
+    results['test/nll_median_{}'.format(intensity)] = median_nll
+    results['test/error_median_{}'.format(intensity)] = median_error
+    results['test/ece_median_{}'.format(intensity)] = median_ece
+    results['test/nll_mean_corrupted'] += avg_nll
+    results['test/ece_mean_corrupted'] += avg_ece
+    results['test/error_mean_corrupted'] += avg_error
+    results['test/nll_median_corrupted'] += median_nll
+    results['test/ece_median_corrupted'] += median_ece
+    results['test/error_median_corrupted'] += median_error
+
+  results['test/nll_mean_corrupted'] /= max_intensity
+  results['test/error_mean_corrupted'] /= max_intensity
+  results['test/ece_mean_corrupted'] /= max_intensity
+  results['test/nll_median_corrupted'] /= max_intensity
+  results['test/error_median_corrupted'] /= max_intensity
+  results['test/ece_median_corrupted'] /= max_intensity
+  return results
+
+
+# TODO(ghassen): disagreement and double_fault could be extended beyond pairs.
+def disagreement(logits_1, logits_2):
+  """Disagreement between the predictions of two classifiers."""
+  preds_1 = tf.argmax(logits_1, axis=-1, output_type=tf.int32)
+  preds_2 = tf.argmax(logits_2, axis=-1, output_type=tf.int32)
+  return tf.reduce_mean(tf.cast(preds_1 != preds_2, tf.float32))
+
+
+def double_fault(logits_1, logits_2, labels):
+  """Double fault[1] is the number of examples both classifiers predict wrong.
+
+  Args:
+    logits_1: tf.Tensor.
+    logits_2: tf.Tensor.
+    labels: tf.Tensor.
+
+  Returns:
+    Scalar double-fault diversity metric.
+
+  ## References
+
+  [1] Kuncheva, Ludmila I., and Christopher J. Whitaker. "Measures of diversity
+      in classifier ensembles and their relationship with the ensemble
+      accuracy." Machine learning 51.2 (2003): 181-207.
+  """
+  preds_1 = tf.cast(tf.argmax(logits_1, axis=-1), labels.dtype)
+  preds_2 = tf.cast(tf.argmax(logits_2, axis=-1), labels.dtype)
+
+  fault_1_idx = tf.squeeze(tf.where(preds_1 != labels))
+  fault_1_idx = tf.cast(fault_1_idx, tf.int32)
+
+  preds_2_at_idx = tf.gather(preds_2, fault_1_idx)
+  labels_at_idx = tf.gather(labels, fault_1_idx)
+
+  double_faults = preds_2_at_idx != labels_at_idx
+  double_faults = tf.cast(double_faults, tf.float32)
+  return tf.reduce_mean(double_faults)
+
+
+def logit_kl_divergence(logits_1, logits_2):
+  """Average KL divergence between logit distributions of two classifiers."""
+  probs_1 = tf.nn.softmax(logits_1)
+  probs_2 = tf.nn.softmax(logits_2)
+  vals = kl_divergence(probs_1, probs_2)
+  return tf.reduce_mean(vals)
+
+
+def kl_divergence(p, q):
+  """Generalized KL divergence [1] for unnormalized distributions.
+
+  Args:
+    p: tf.Tensor.
+    q: tf.Tensor
+
+  Returns:
+    tf.Tensor of the Kullback-Leibler divergences per example.
+
+  ## References
+
+  [1] Lee, Daniel D., and H. Sebastian Seung. "Algorithms for non-negative
+  matrix factorization." Advances in neural information processing systems.
+  2001.
+  """
+  return tf.reduce_sum(p * tf.math.log(p / q) - p + q, axis=-1)
+
+
+def lp_distance(x, y, p=1):
+  """l_p distance."""
+  diffs_abs = tf.abs(x - y)
+  summation = tf.reduce_sum(tf.math.pow(diffs_abs, p), axis=-1)
+  return tf.reduce_mean(tf.math.pow(summation, 1./p), axis=-1)
+
+
+def cosine_distance(x, y):
+  """Cosine distance between vectors x and y."""
+  x_norm = tf.math.sqrt(tf.reduce_sum(tf.pow(x, 2), axis=-1))
+  x_norm = tf.reshape(x_norm, (-1, 1))
+  y_norm = tf.math.sqrt(tf.reduce_sum(tf.pow(y, 2), axis=-1))
+  y_norm = tf.reshape(y_norm, (-1, 1))
+  normalized_x = x / x_norm
+  normalized_y = y / y_norm
+  return tf.reduce_mean(tf.reduce_sum(normalized_x * normalized_y, axis=-1))
+
+
+# TODO(ghassen): we could extend this to take an arbitrary list of metric fns.
+def average_pairwise_diversity(probs, num_models):
+  """Average pairwise distance computation across models."""
+  if probs.shape[0] != num_models:
+    raise ValueError('The number of models {0} does not match '
+                     'the probs length {1}'.format(num_models, probs.shape[0]))
+
+  pairwise_disagreement = []
+  pairwise_kl_divergence = []
+  pairwise_cosine_distance = []
+  for pair in list(itertools.combinations(range(num_models), 2)):
+    probs_1 = probs[pair[0]]
+    probs_2 = probs[pair[1]]
+    pairwise_disagreement.append(disagreement(probs_1, probs_2))
+    pairwise_kl_divergence.append(
+        tf.reduce_mean(kl_divergence(probs_1, probs_2)))
+    pairwise_cosine_distance.append(cosine_distance(probs_1, probs_2))
+
+  # TODO(ghassen): we could also return max and min pairwise metrics.
+  average_disagreement = tf.reduce_mean(tf.stack(pairwise_disagreement))
+  average_kl_divergence = tf.reduce_mean(tf.stack(pairwise_kl_divergence))
+  average_cosine_distance = tf.reduce_mean(tf.stack(pairwise_cosine_distance))
+
+  return {
+      'disagreement': average_disagreement,
+      'average_kl': average_kl_divergence,
+      'cosine_similarity': average_cosine_distance
+  }
