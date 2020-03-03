Fix Flipout's sign flips to be actual random signs.
As a TODO, I'll retune the CIFAR variational BNNs.

PiperOrigin-RevId: 298510917

edward2/tensorflow/layers/convolutional.py
@@ -164,14 +164,16 @@ class Conv2DFlipout(Conv2DReparameterization):
       channels = input_shape[-1]
       sign_input_shape = [batch_dim, 1, 1, channels]
       sign_output_shape = [batch_dim, 1, 1, self.filters]
-    sign_input = 2 * tf.random.uniform(sign_input_shape,
-                                       minval=0,
-                                       maxval=2,
-                                       dtype=inputs.dtype) - 1
-    sign_output = 2 * tf.random.uniform(sign_output_shape,
-                                        minval=0,
-                                        maxval=2,
-                                        dtype=inputs.dtype) - 1
+    sign_input = tf.cast(2 * tf.random.uniform(sign_input_shape,
+                                               minval=0,
+                                               maxval=2,
+                                               dtype=tf.int32) - 1,
+                         inputs.dtype)
+    sign_output = tf.cast(2 * tf.random.uniform(sign_output_shape,
+                                                minval=0,
+                                                maxval=2,
+                                                dtype=tf.int32) - 1,
+                          inputs.dtype)
     kernel_mean = self.kernel.distribution.mean()
     perturbation = self.kernel - kernel_mean
     outputs = self._convolution_op(inputs, kernel_mean)

edward2/tensorflow/layers/dense.py
@@ -259,15 +259,17 @@ class DenseFlipout(DenseReparameterization):
       return super(DenseFlipout, self).call(inputs)
     self.call_weights()
     input_shape = tf.shape(inputs)
-    sign_input = 2 * tf.random.uniform(input_shape,
-                                       minval=0,
-                                       maxval=2,
-                                       dtype=inputs.dtype) - 1
-    sign_output = 2 * tf.random.uniform(tf.concat([input_shape[:-1],
-                                                   [self.units]], 0),
-                                        minval=0,
-                                        maxval=2,
-                                        dtype=inputs.dtype) - 1
+    output_shape = tf.concat([input_shape[:-1], [self.units]], 0)
+    sign_input = tf.cast(2 * tf.random.uniform(input_shape,
+                                               minval=0,
+                                               maxval=2,
+                                               dtype=tf.int32) - 1,
+                         inputs.dtype)
+    sign_output = tf.cast(2 * tf.random.uniform(output_shape,
+                                                minval=0,
+                                                maxval=2,
+                                                dtype=inputs.dtype) - 1,
+                          inputs.dtype)
     kernel_mean = self.kernel.distribution.mean()
     perturbation = self.kernel - kernel_mean
     if inputs.shape.ndims <= 2:

edward2/tensorflow/layers/recurrent.py
@@ -208,13 +208,17 @@ class LSTMCellFlipout(LSTMCellReparameterization):
       dtype = inputs.dtype
     input_dim = tf.shape(self.kernel)[0]
     self.sign_input = 2 * tf.random.uniform(
-        [batch_size, 4 * input_dim], minval=0, maxval=2, dtype=dtype) - 1
+        [batch_size, 4 * input_dim], minval=0, maxval=2, dtype=tf.int32) - 1
     self.sign_output = 2 * tf.random.uniform(
-        [batch_size, 4 * self.units], minval=0, maxval=2, dtype=dtype) - 1
+        [batch_size, 4 * self.units], minval=0, maxval=2, dtype=tf.int32) - 1
     self.recurrent_sign_input = 2 * tf.random.uniform(
-        [batch_size, 4 * self.units], minval=0, maxval=2, dtype=dtype) - 1
+        [batch_size, 4 * self.units], minval=0, maxval=2, dtype=tf.int32) - 1
     self.recurrent_sign_output = 2 * tf.random.uniform(
-        [batch_size, 4 * self.units], minval=0, maxval=2, dtype=dtype) - 1
+        [batch_size, 4 * self.units], minval=0, maxval=2, dtype=tf.int32) - 1
+    self.sign_input = tf.cast(self.sign_input, dtype)
+    self.sign_output = tf.cast(self.sign_output, dtype)
+    self.recurrent_sign_input = tf.cast(self.recurrent_sign_input, dtype)
+    self.recurrent_sign_output = tf.cast(self.recurrent_sign_output, dtype)
 
   def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
     """Get the initial state and side-effect sampling of stochastic weights."""