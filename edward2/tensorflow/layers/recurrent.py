Fix a bug in the LSTMCellFlipout layer involving incorrect shapes.
In the flipout version of the stochastic LSTM cell, the input sign
vector (as part of the rank-1 reparameterization) had an incorrect
shape of (batch_size, 4 * input_dim). Since the kernel is of shape
(input_dim, 4 * units), the correct shape is (batch_size, input_dim).
Similarly, the recurrent input sign vector had a shape of
(batch_size, 4 * units) when it should be (batch_size, units).

When implementation=2, this bug was readily apparent.

PiperOrigin-RevId: 298650518

edward2/tensorflow/layers/recurrent.py
@@ -208,11 +208,11 @@ class LSTMCellFlipout(LSTMCellReparameterization):
       dtype = inputs.dtype
     input_dim = tf.shape(self.kernel)[0]
     self.sign_input = 2 * tf.random.uniform(
-        [batch_size, 4 * input_dim], minval=0, maxval=2, dtype=tf.int32) - 1
+        [batch_size, input_dim], minval=0, maxval=2, dtype=tf.int32) - 1
     self.sign_output = 2 * tf.random.uniform(
         [batch_size, 4 * self.units], minval=0, maxval=2, dtype=tf.int32) - 1
     self.recurrent_sign_input = 2 * tf.random.uniform(
-        [batch_size, 4 * self.units], minval=0, maxval=2, dtype=tf.int32) - 1
+        [batch_size, self.units], minval=0, maxval=2, dtype=tf.int32) - 1
     self.recurrent_sign_output = 2 * tf.random.uniform(
         [batch_size, 4 * self.units], minval=0, maxval=2, dtype=tf.int32) - 1
     self.sign_input = tf.cast(self.sign_input, dtype)
@@ -239,18 +239,16 @@ class LSTMCellFlipout(LSTMCellReparameterization):
     perturbation = self.recurrent_kernel - kernel_mean
     k_i, k_f, k_c, k_o = tf.split(kernel_mean, num_or_size_splits=4, axis=1)
     p_i, p_f, p_c, p_o = tf.split(perturbation, num_or_size_splits=4, axis=1)
-    si_i, si_f, si_c, si_o = tf.split(self.recurrent_sign_input,
-                                      num_or_size_splits=4, axis=1)
     so_i, so_f, so_c, so_o = tf.split(self.recurrent_sign_output,
                                       num_or_size_splits=4, axis=1)
     z0 = (x_i + tf.keras.backend.dot(h_tm1_i, k_i) +
-          tf.keras.backend.dot(h_tm1_i * si_i, p_i) * so_i)
+          tf.keras.backend.dot(h_tm1_i * self.recurrent_sign_input, p_i) * so_i)
     z1 = (x_f + tf.keras.backend.dot(h_tm1_f, k_f) +
-          tf.keras.backend.dot(h_tm1_f * si_f, p_f) * so_f)
+          tf.keras.backend.dot(h_tm1_f * self.recurrent_sign_input, p_f) * so_f)
     z2 = (x_c + tf.keras.backend.dot(h_tm1_c, k_c) +
-          tf.keras.backend.dot(h_tm1_c * si_c, p_c) * so_c)
+          tf.keras.backend.dot(h_tm1_c * self.recurrent_sign_input, p_c) * so_c)
     z3 = (x_o + tf.keras.backend.dot(h_tm1_o, k_o) +
-          tf.keras.backend.dot(h_tm1_o * si_o, p_o) * so_o)
+          tf.keras.backend.dot(h_tm1_o * self.recurrent_sign_input, p_o) * so_o)
     i = self.recurrent_activation(z0)
     f = self.recurrent_activation(z1)
     c = f * c_tm1 + i * self.activation(z2)
@@ -289,18 +287,16 @@ class LSTMCellFlipout(LSTMCellReparameterization):
       perturbation = self.kernel - kernel_mean
       k_i, k_f, k_c, k_o = tf.split(kernel_mean, num_or_size_splits=4, axis=1)
       p_i, p_f, p_c, p_o = tf.split(perturbation, num_or_size_splits=4, axis=1)
-      si_i, si_f, si_c, si_o = tf.split(self.sign_input,
-                                        num_or_size_splits=4, axis=1)
       so_i, so_f, so_c, so_o = tf.split(self.sign_output,
                                         num_or_size_splits=4, axis=1)
       x_i = (tf.keras.backend.dot(inputs_i, k_i) +
-             tf.keras.backend.dot(inputs_i * si_i, p_i) * so_i)
+             tf.keras.backend.dot(inputs_i * self.sign_input, p_i) * so_i)
       x_f = (tf.keras.backend.dot(inputs_f, k_f) +
-             tf.keras.backend.dot(inputs_f * si_f, p_f) * so_f)
+             tf.keras.backend.dot(inputs_f * self.sign_input, p_f) * so_f)
       x_c = (tf.keras.backend.dot(inputs_c, k_c) +
-             tf.keras.backend.dot(inputs_c * si_c, p_c) * so_c)
+             tf.keras.backend.dot(inputs_c * self.sign_input, p_c) * so_c)
       x_o = (tf.keras.backend.dot(inputs_o, k_o) +
-             tf.keras.backend.dot(inputs_o * si_o, p_o) * so_o)
+             tf.keras.backend.dot(inputs_o * self.sign_input, p_o) * so_o)
       if self.use_bias:
         b_i, b_f, b_c, b_o = tf.split(
             self.bias, num_or_size_splits=4, axis=0)

edward2/tensorflow/layers/recurrent_test.py
@@ -32,36 +32,67 @@ class RecurrentTest(parameterized.TestCase, tf.test.TestCase):
        "kernel_initializer": "zeros",
        "recurrent_initializer": "orthogonal",
        "bias_initializer": "zeros",
+       "implementation": 1,
        "all_close": True},
       {"lstm_cell": ed.layers.LSTMCellFlipout,
        "kernel_initializer": "trainable_normal",
        "recurrent_initializer": "orthogonal",
        "bias_initializer": "zeros",
+       "implementation": 1,
        "all_close": False},
       {"lstm_cell": ed.layers.LSTMCellFlipout,
        "kernel_initializer": "zeros",
        "recurrent_initializer": "orthogonal",
        "bias_initializer": "trainable_normal",
+       "implementation": 1,
+       "all_close": False},
+      {"lstm_cell": ed.layers.LSTMCellFlipout,
+       "kernel_initializer": "zeros",
+       "recurrent_initializer": "orthogonal",
+       "bias_initializer": "zeros",
+       "implementation": 2,
+       "all_close": True},
+      {"lstm_cell": ed.layers.LSTMCellFlipout,
+       "kernel_initializer": "trainable_normal",
+       "recurrent_initializer": "orthogonal",
+       "bias_initializer": "zeros",
+       "implementation": 2,
+       "all_close": False},
+      {"lstm_cell": ed.layers.LSTMCellFlipout,
+       "kernel_initializer": "zeros",
+       "recurrent_initializer": "orthogonal",
+       "bias_initializer": "trainable_normal",
+       "implementation": 2,
        "all_close": False},
       {"lstm_cell": ed.layers.LSTMCellReparameterization,
        "kernel_initializer": "zeros",
        "recurrent_initializer": "orthogonal",
        "bias_initializer": "zeros",
+       "implementation": 1,
        "all_close": True},
       {"lstm_cell": ed.layers.LSTMCellReparameterization,
        "kernel_initializer": "trainable_normal",
        "recurrent_initializer": "orthogonal",
        "bias_initializer": "zeros",
+       "implementation": 1,
        "all_close": False},
       {"lstm_cell": ed.layers.LSTMCellReparameterization,
        "kernel_initializer": "zeros",
        "recurrent_initializer": "trainable_normal",
        "bias_initializer": "zeros",
+       "implementation": 1,
        "all_close": False},
       {"lstm_cell": ed.layers.LSTMCellReparameterization,
        "kernel_initializer": "zeros",
        "recurrent_initializer": "orthogonal",
        "bias_initializer": "trainable_normal",
+       "implementation": 1,
+       "all_close": False},
+      {"lstm_cell": ed.layers.LSTMCellReparameterization,
+       "kernel_initializer": "trainable_normal",
+       "recurrent_initializer": "orthogonal",
+       "bias_initializer": "zeros",
+       "implementation": 2,
        "all_close": False},
   )
   def testLSTMCell(self,
@@ -69,21 +100,23 @@ class RecurrentTest(parameterized.TestCase, tf.test.TestCase):
                    kernel_initializer,
                    recurrent_initializer,
                    bias_initializer,
+                   implementation,
                    all_close):
-    batch_size, timesteps, dim = 5, 3, 12
+    batch_size, dim = 5, 12
     hidden_size = 10
-    inputs = np.random.rand(batch_size, timesteps, dim).astype(np.float32)
+    inputs = np.random.rand(batch_size, dim).astype(np.float32)
     cell = lstm_cell(hidden_size,
                      kernel_initializer=kernel_initializer,
                      recurrent_initializer=recurrent_initializer,
-                     bias_initializer=bias_initializer)
+                     bias_initializer=bias_initializer,
+                     implementation=implementation)
     noise = np.random.rand(1, hidden_size).astype(np.float32)
     h0, c0 = cell.get_initial_state(inputs)
     state = (h0 + noise, c0)
-    outputs1, _ = cell(inputs[:, 0, :], state)
-    outputs2, _ = cell(inputs[:, 0, :], state)
+    outputs1, _ = cell(inputs, state)
+    outputs2, _ = cell(inputs, state)
     cell.call_weights()
-    outputs3, _ = cell(inputs[:, 0, :], state)
+    outputs3, _ = cell(inputs, state)
     self.assertEqual(outputs1.shape, (batch_size, hidden_size))
     self.assertAllClose(outputs1, outputs2)
     if all_close: