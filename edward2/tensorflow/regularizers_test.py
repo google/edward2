9bdc851ecd64d62a926d13a8852dfaf8dcabdacd
Mike Dusenberry <dusenberrymw@google.com>
2020-03-26 18:51:59 -0700

    internal
    PiperOrigin-RevId: 303239127

--- edward2/tensorflow/regularizers_test.py
+++ edward2/tensorflow/regularizers_test.py
@@ -184,6 +184,7 @@ class RegularizersTest(parameterized.TestCase, tf.test.TestCase):
     self.assertIsInstance(ed.regularizers.get('normal_kl_divergence'),
                           ed.regularizers.NormalKLDivergence)
     self.assertIsInstance(ed.regularizers.get('l2'), tf.keras.regularizers.L1L2)
+    self.assertIsNone(ed.regularizers.get(''))
 
 if __name__ == '__main__':
   tf.enable_v2_behavior()
