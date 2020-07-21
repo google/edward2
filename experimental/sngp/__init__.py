a1deee1f63aa2271bc40731dfceb7e7852a9c38a
Jeremiah Liu <jereliu@google.com>
2020-07-20 19:42:18 -0700

    Add package-level import for SNGP.
    PiperOrigin-RevId: 322276743

---
+++ experimental/sngp/__init__.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+# Copyright 2020 The Edward2 Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Spectral-normalized neural Gaussian process (SNGP)."""
+
+# pylint: disable=wildcard-import
+from experimental.sngp.gaussian_process import *  # local file import
+from experimental.sngp.normalization import *  # local file import
+# pylint: enable=wildcard-import
