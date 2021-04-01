18080bf07f54eac5b25b8fbf3655f20a910384f9
Edward2 Team <no-reply@google.com>
2021-04-01 02:00:05 -0700

    Fix ECE metric update function syntax inside ensembles loop. Add MultiHeadMCSoftmaxDenseFA name in layers/__init__.py
    PiperOrigin-RevId: 366208245

--- edward2/tensorflow/layers/__init__.py
+++ edward2/tensorflow/layers/__init__.py
@@ -118,6 +118,7 @@ __all__ = [
     "MCSigmoidDenseFA",
     "MCSoftmaxDense",
     "MCSoftmaxDenseFA",
+    "MultiHeadMCSoftmaxDenseFA",
     "MixtureLogistic",
     "NCPCategoricalPerturb",
     "NCPNormalOutput",
