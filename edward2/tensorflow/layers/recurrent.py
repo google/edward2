78ad13f70916db3fce5e8558cdc8ec2f34e775b3
Yilei Yang <yileiyang@google.com>
2022-03-24 08:58:33 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 437006356

--- edward2/tensorflow/layers/recurrent.py
+++ edward2/tensorflow/layers/recurrent.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Bayesian recurrent cells and layers."""
 
 from typing import List, Tuple, Union  # pylint:disable=g-bad-import-order
