a2f18afd3df009c3a9f5edcd6eed623fc5caaf3b
Yilei Yang <yileiyang@google.com>
2022-04-19 08:37:35 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 442820319

--- experimental/sngp/normalization.py
+++ experimental/sngp/normalization.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Implementation of spectral normalization for Dense and Conv2D layers.
 
 ## References:
